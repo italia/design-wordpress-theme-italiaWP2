ad04f6f10903726279fd32bee7e136092d54d966
Gregorio Galante <me@gregoriogalante.com>
2021-01-09 10:20:29 +0100

    Ottimizzazione stile ricerca header

--- italiawp2/searchform.php
+++ italiawp2/searchform.php
@@ -1,6 +1,6 @@
 <div class="cerca">
 <form class="Form" method="get" role="search" action="<?php echo esc_url(home_url('/')); ?>">
-    <input type="text" value="" name="s" title="<?php echo __('Start the search','italiawp2'); ?>" required>
+    <input type="text" value="" name="s" title="<?php echo __('Start the search','italiawp2'); ?>" placeholder="<?php echo __('Start the search','italiawp2'); ?>" required>
     <button class="btn btn-default btn-cerca pull-right" name="submit" title="<?php echo __('Start the search','italiawp2'); ?>" aria-label="<?php echo __('Start the search','italiawp2'); ?>">
         <svg class="icon">
         <use xlink:href="<?php bloginfo('template_url'); ?>/static/img/ponmetroca.svg#ca-search"></use>
